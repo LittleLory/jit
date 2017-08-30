48f0b681de4d3bc061bd3a213fe793ba9eb6ccc6
littlelory <303548901@qq.com>
2017-08-30 16:17:03 +0800

    fix the bug that fingerprint contains space when the input is a byte array which is not generate from a String

--- src/main/java/cn/littlelory/Fingerprint.java
+++ src/main/java/cn/littlelory/Fingerprint.java
@@ -31,11 +31,12 @@ class Fingerprint {
         }
     }
 
+    private static final char[] map = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
     private static String bytesToHexStr(byte[] bytes) {
         char[] chars = new char[bytes.length*2];
         for (int i = 0; i < bytes.length; i++) {
-            chars[2*i] = (char) (bytes[i] >> 4 & 0xf);
-            chars[2*i+1] = (char) (bytes[i] & 0xf);
+            chars[2*i] = map[bytes[i] >> 4 & 0xf];
+            chars[2*i+1] = map[bytes[i] & 0xf];
         }
         return String.valueOf(chars);
     }
--- src/test/java/cn/littlelory/FingerprintTest.java
+++ src/test/java/cn/littlelory/FingerprintTest.java
@@ -9,11 +9,25 @@ import static org.junit.Assert.assertEquals;
  */
 public class FingerprintTest {
     @Test
-    public void generate() {
+    public void generate_from_str() {
         String value = "JitBeanName";
 
         String expect = "8fc6588f3e1d02875766ece925051422d7bf41bf";
         String actual = Fingerprint.generate(value.getBytes());
+
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    public void generate_from_bytes() {
+        byte[] bytes = new byte[4];
+        bytes[3] = (byte) 0x00;
+        bytes[2] = (byte) 0x00;
+        bytes[1] = (byte) 0x00;
+        bytes[0] = (byte) 0x80;
+
+        String expect = "e1798367d1b1ed54425e99f790a986c1fe939414";
+        String actual = Fingerprint.generate(bytes);
 
         assertEquals(expect, actual);
     }
